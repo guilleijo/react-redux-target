f46bfa0b37dc49c0088e809af834e3f66a4f75c0
Guillermo Eijo <guillermoeijo@gmail.com>
2019-07-30 17:13:35 -0300

    Login initial structure and styles

--- src/containers/LoginPage.js
+++ src/containers/LoginPage.js
@@ -1,11 +1,13 @@
 import React, { memo } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
+import right from 'assets/right.jpg';
 
 import { useSession, useDispatch } from 'hooks';
 import LoginForm from 'components/user/LoginForm';
 import { login } from 'actions/sessionActions';
 import routes from 'constants/routesPaths';
+import Divider from '../components/common/Divider';
 
 const LoginPage = () => {
   const { authenticated } = useSession();
@@ -16,14 +18,29 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
-      <p>
-        <FormattedMessage id="login.title" />
-      </p>
-      <LoginForm onSubmit={loginRequest} />
-      <Link to={routes.signUp}>
-        <FormattedMessage id="login.signup" />
-      </Link>
+    <div className="wrapper center">
+      <div className="sign-up-form left">
+        <p className="title" style={{ marginBottom: 38 }}>
+          <FormattedMessage id="login.title" />
+        </p>
+        <LoginForm onSubmit={loginRequest} />
+        <Divider />
+        <div className="form-label">
+          <Link className="link" to={routes.signUp}>
+            <FormattedMessage id="login.signup" />
+          </Link>
+        </div>
+      </div>
+      <div
+        className="right"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <img src={right} alt="Right" />
+      </div>
     </div>
   );
 };
